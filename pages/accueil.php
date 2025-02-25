4daf2d345b5042ff4a351fbe2c8b8acadddf2e79
v-alu-elderblood <lorenzo.bellier@gmail.com>
2025-02-25 09:54:05 +0100

    feat : ajout accueil.php + le style

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,1 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>To-do list</title>
+
+    <!-- lien bootstrap -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="../assets/style.css">
+
+    <link rel="shortcut icon" href="../assets/img/japan_list.png" type="image/x-icon">
+</head>
+<body class="accueil_body">
+
+<nav class="navbar navbar-expand-lg" style="background-color: #fcc9b9;">
+    <!-- Logo avec lien vers l'accueil -->
+    <a class="navbar-brand" href="../index.html">
+        <img src="../assets/img/japan_list.png" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- Les boutons à droite (Inscription et Connexion) -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="inscription.php" class="btn btn-inscription" role="button" style="background-color: #DB5A6B;">liste des to-do list</a>
+            </li>
+            <li class="nav-item">
+                <a href="connexion" class="btn btn-connexion" role="button" style="background-color: #b3bcff;">Déconnexion</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Popper.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        // Code pour afficher ou masquer le menu burger
+        $('.navbar-toggler').click(function() {
+            $('#navbarNav').toggleClass('collapse');
+        });
+    });
+</script>
+
+</body>
+</html>
